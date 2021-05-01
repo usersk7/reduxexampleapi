444e088e7ded48682e5df8824741a4844b3b3620
usersk7 <shubhamkumbhar787@gmail.com>
2021-05-01 15:57:47 +0530

    Tutorial API with SpringBoot Mysql  - CRUD oprtaions  - Integration with react web

---
+++ src/main/java/com/reduxexampleapi/ReduxexampleapiApplication.java
@@ -0,0 +1,13 @@
+package com.reduxexampleapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReduxexampleapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReduxexampleapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/reduxexampleapi/ReduxexampleapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reduxexampleapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReduxexampleapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
